72b7d8327836a766e836da8b372a38ef4432dc4d
Andre Weinand <aweinand@microsoft.com>
2015-12-28 15:52:14 +0100

    change SourceBreakpoint attribute 'expression' to 'condition'

--- protocol/src/debugProtocol.ts
+++ protocol/src/debugProtocol.ts
@@ -500,7 +500,7 @@ export module DebugProtocol {
 		/** An optional source column of the breakpoint. */
 		column?: number;
 		/** An optional expression for conditional breakpoints. */
-		expression?: string;
+		condition?: string;
 	}
 
 	/** Information about a Breakpoint created in the setBreakpoints request.
