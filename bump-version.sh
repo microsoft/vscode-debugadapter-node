556f02ca7a693bfa10ce6ab8a8142ee813cb1a07
Connor Peet <connor@peet.io>
2022-12-13 09:45:11 -0800

    make bump version better

--- bump-version.sh
+++ bump-version.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 npm version --workspaces "$@"
-git add .
 
 VERSION=v$(cat adapter/package.json | jq -r .version)
+git checkout -b release/$VERSION
+
+git add .
 git commit -m $VERSION
+git push -u origin release/$VERSION
+
 git tag $VERSION
-# git push --tags
+git push origin $VERSION
