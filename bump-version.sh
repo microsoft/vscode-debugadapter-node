34a6a5536be25c3517fccf98aebf98e6595f0a09
Connor Peet <connor@peet.io>
2023-06-02 15:12:14 -0700

    fix: version bump script should sync package versions

--- bump-version.sh
+++ bump-version.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-npm version --workspaces "$@"
+npm version --workspaces "$@" --workspaces-update
 
 VERSION=v$(cat adapter/package.json | jq -r .version)
 git checkout -b release/$VERSION
